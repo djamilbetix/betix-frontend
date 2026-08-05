bb25e29c98fe2eaa800225e66dff19272a29b2b4
djamilbetix <fennecfg@gmail.com>
2026-08-05 11:24:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1291,8 +1291,8 @@ function generateTicketQR(ticketId) {
     try {
         new QRCode(container, {
             text: ticket.qrCode || ticket.id,
-            width: 88,
-            height: 88,
+            width: 82,
+            height: 82,
             colorDark: "#0B1F5C",
             colorLight: "#ffffff",
             correctLevel: QRCode.CorrectLevel.H
